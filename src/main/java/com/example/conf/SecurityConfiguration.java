cfdb29b7c98beeccf09f18d197f2f9c7a70705ee
de2179 <benjamin.hoffmann@kiongroup.com>
2017-03-23 23:32:08 +0100

    Added OpenID / Adal Authentication

---
+++ src/main/java/com/example/conf/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.example.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.example.security.AdalSecurityFilter;
+
+/**
+ * @author de2179
+ *
+ */
+@Configuration()
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	    @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	            .authorizeRequests()
+	            	.antMatchers("/","/**").permitAll()
+	                .anyRequest().authenticated()
+	                .and()
+	            .logout()
+	                .permitAll();
+	    }
+
+	    @Bean
+	    public AdalSecurityFilter adalSecurityFilter() {
+	      return new AdalSecurityFilter();
+	    }
+
+	    @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	        auth
+	            .inMemoryAuthentication()
+	                .withUser("user").password("password").roles("USER");
+	    }
+	
+}
